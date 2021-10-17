10130641bdf5c184a8a6e05c8f89cd1665fec5cc
ikanx101 <ikanx101@gmail.com>
2021-10-17 07:43:15 +0000

    contour

---
+++ Semester I/Analisis Numerik Lanjut/Tugas Kerang/tes.R
@@ -0,0 +1,20 @@
+rm(list=ls())
+
+library(dplyr)
+library(ggplot2)
+
+f1 = function(x1,x2){cos(2* x1) - cos(2*x2) - 0.4}
+
+ikanx = function(f,a,b){
+  x = seq(a,b,by = .25)
+
+  temp_plot = 
+    expand.grid(x,x) %>%
+    rename(x = Var1,
+	   y = Var2) %>%
+    mutate(z = f(x,y))
+
+  return(temp_plot)
+}
+
+
