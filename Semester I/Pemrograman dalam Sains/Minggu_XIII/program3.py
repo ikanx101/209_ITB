09decb73711d344c30c031d6119b646b0a6a5124
ikanx101 <ikanx101@gmail.com>
2021-11-23 14:03:44 +0700

    uji coba

---
+++ Semester I/Pemrograman dalam Sains/Minggu_XIII/program3.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-poster')
+# matplotlib inline
+
+x = np.arange(0, 2*np.pi, 0.01) 
+# compute function
+omega = 100
+epsilon = 0.01
+
+y = np.cos(x) 
+y_noise = y + epsilon*np.sin(omega*x)
+
+# Plot solution
+plt.figure(figsize = (12, 8))
+plt.plot(x, y_noise, 'r-', \
+         label = 'cos(x) + noise')
+plt.plot(x, y, 'b-', \
+         label = 'cos(x)')
+
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.legend()
+#plt.show()
+plt.savefig('plot3.png',dpi = 450)
---
+++ Semester I/Pemrograman dalam Sains/Minggu_XIII/program4.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-poster')
+
+# compute function
+omega = 100
+epsilon = 0.01
+
+x = np.arange(0, 2*np.pi, 0.01) 
+# compute function
+y = -np.sin(x) 
+y_noise = y + epsilon*omega*np.cos(omega*x)
+
+# Plot solution
+plt.figure(figsize = (12, 8))
+plt.plot(x, y_noise, 'r-', \
+         label = 'Derivative cos(x) + noise')
+plt.plot(x, y, 'b-', \
+         label = 'Derivative of cos(x)')
+
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.legend()
+#plt.show()
+plt.savefig('plot4.png',dpi = 450)
