c6054b1d8a10104261f642ac0410500d40b42ad9
ikanx101 <ikanx101@gmail.com>
2022-05-18 20:01:28 +0700

    presentasi research method

--- Semester II/Research Method/UAS/algoritma finale.R
+++ Semester II/Research Method/UAS/algoritma finale.R
@@ -1,14 +1,17 @@
-setwd("/cloud/project/Semester II/Research Method/UAS")
+setwd("~/209_ITB/Semester II/Research Method/UAS")
 
 rm(list=ls())
 
 library(dplyr)
 
-rdas = list.files("/cloud/project/Semester II/Research Method/UAS/Hasil Perhitungan/",full.names = T)
+rdas = list.files("~/209_ITB/Semester II/Research Method/UAS/Hasil Perhitungan/",full.names = T)
 
 ompr = vector("list",10)
 spiral = vector("list",10)
 simulasi = vector("list",10)
+sama = rep(NA,10)
+ompr_sum = rep(NA,10)
+spiral_sum = rep(NA,10)
 
 i = 1
 
@@ -33,6 +36,10 @@ for(i in 1:10){
     summarise(budget = sum(burn_3m),
               revenue = sum(cost_benefit))
   
+  sama[i] = sum(df_hit$pilih_ompr == df_hit$pilih_spiral)
+  
+  ompr_sum[i] = sum(df_hit$pilih_ompr)
+  spiral_sum[i] = sum(df_hit$pilih_spiral)
 }
 
 # ==============================
@@ -43,9 +50,12 @@ gabung_simulasi = do.call(rbind,simulasi)
 gabung_all = cbind(gabung_ompr,gabung_simulasi,gabung_spiral)
 colnames(gabung_all)[1:2] = paste0("eksak_",colnames(gabung_all)[1:2])
 colnames(gabung_all)[3:4] = paste0("existing_",colnames(gabung_all)[3:4])
-colnames(gabung_all)[5:6] = paste0("spiral__",colnames(gabung_all)[5:6])
+colnames(gabung_all)[5:6] = paste0("spiral_",colnames(gabung_all)[5:6])
 
 save(gabung_all,
+     sama,
+     ompr_sum,
+     spiral_sum,
      file = "bahan review.rda")
 
 
