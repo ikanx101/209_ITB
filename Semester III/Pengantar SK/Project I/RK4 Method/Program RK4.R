449fb436a69fa3224e60a50ffe4a35b5a66a37c6
mr.ikanx <ikanx101@gmail.com>
2022-10-21 09:03:52 +0000

    update RK4

--- Semester III/Pengantar SK/Project I/RK4 Method/Program RK4.R
+++ Semester III/Pengantar SK/Project I/RK4 Method/Program RK4.R
@@ -8,56 +8,71 @@ q_b = 0   # massa awal zat radioaktif B
 q_c = 0   # massa awal zat radioaktif C
 t   = 0   # waktu awal t = 0
 
-dt = .25           # delta t
 iter_length = 5    # panjang iterasi
-num_iter = (iter_length / dt) + 1 # berapa banyak iterasi 
 h = .01
-
-d_a = function(q_a,t){
-  (-r_a * q_a) * dt
-  }
-d_b = function(q_a,q_b,t){
-  ((r_a * q_a) - (r_b * q_b)) * dt
-  }
-d_c = function(q_b,q_c,t){
-  ((r_b * q_b) - (r_c * q_c)) * dt
-  }
+num_iter = (iter_length / h) + 1 # berapa banyak iterasi 
 
 
-for(i in 1:num_iter){
+d_a = function(t,q_a){
+  (-r_a * q_a) 
+  }
+d_b = function(t,q_a,q_b){
+  ((r_a * q_a) - (r_b * q_b)) 
+  }
+d_c = function(t,q_b,q_c){
+  ((r_b * q_b) - (r_c * q_c)) 
+  }
+
+
+A = c(q_a)
+B = c(q_b)
+C = c(q_c)
+t_ = c(t)
+
+
+for(i in 2:num_iter){
   # kita akan hitung dulu zat a
-  x = q_a
-  y = t
+  k1 = d_a(t,A[i-1]) 
+  k2 = d_a(t + 0.5*h,A[i-1] + 0.5*k1*h) 
+  k3 = d_a(t + 0.5*h,A[i-1] + 0.5*k2*h) 
+  k4 = d_a(t + h,A[i-1] + k3*h) 
   
-  k1 = d_a(x0,y0)
-  k2 = d_a(x0 + 0.5*h,y0 + 0.5*k1*h)
-  k3 = d_a(x0 + 0.5*h,y0 + 0.5*k2*h)
-  k4 = d_a(x0 + h,y0 + k3*h)
-  y0 = y0 + (1/6)*(k1 + 2*k2 + 2*k3 + k4) * h
+  A[i] = A[i-1] + (1/6)*(k1 + 2*k2 + 2*k3 + k4) * h
   
-  t = t + h
+  # kita akan hitung zat b
+  k1 = d_b(t,
+           A[i-1],
+           B[i-1]) 
+  k2 = d_b(t + 0.5*h,
+           A[i-1] + 0.5*k1*h,
+           B[i-1] + 0.5*k1*h) 
+  k3 = d_b(t + 0.5*h,
+           A[i-1] + 0.5*k2*h,
+           B[i-1] + 0.5*k2*h) 
+  k4 = d_b(t + h,
+           A[i-1] + k3*h,
+           B[i-1] + k3*h) 
   
-  x = c(x, x0)
-  y = c(y, y0)
+  B[i] = B[i-1] + (1/6)*(k1 + 2*k2 + 2*k3 + k4) * h
+  
+  # kita akan hitung zat c
+  k1 = d_c(t,
+           B[i-1],
+           C[i-1]) 
+  k2 = d_c(t + 0.5*h,
+           B[i-1] + 0.5*k1*h,
+           C[i-1] + 0.5*k1*h) 
+  k3 = d_c(t + 0.5*h,
+           B[i-1] + 0.5*k2*h,
+           C[i-1] + 0.5*k2*h) 
+  k4 = d_c(t + h,
+           B[i-1] + k3*h,
+           C[i-1] + k3*h) 
+  
+  C[i] = C[i-1] + (1/6)*(k1 + 2*k2 + 2*k3 + k4) * h
+  
+  t_[i] = t_[i-1] + h
+  
 }
 
-rk_4order = function(f, x0, y0, h, n){
-  # initial condition
-  x = x0
-  y = y0
-  # proses iterasi
-  for(i in 1:n){
-    k1 = f(x0,y0)
-    k2 = f(x0 + 0.5*h,y0 + 0.5*k1*h)
-    k3 = f(x0 + 0.5*h,y0 + 0.5*k2*h)
-    k4 = f(x0 + h,y0 + k3*h)
-    y0 = y0 + (1/6)*(k1 + 2*k2 + 2*k3 + k4) * h
-    x0 = x0 + h
-    x = c(x, x0)
-    y = c(y, y0)
-  }
-  # output
-  output = data.frame(x = x,
-                      y = y)
-  return(output)
-}
+data.frame(t_,A,B,C) %>% tail()
