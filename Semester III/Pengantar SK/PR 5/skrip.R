8556b9ee63efe803845e7a263cb4eda5cd743928
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-11-23 06:23:35 +0700

    baru save segini doank

--- Semester III/Pengantar SK/PR 5/skrip.R
+++ Semester III/Pengantar SK/PR 5/skrip.R
@@ -5,27 +5,9 @@ rm(list=ls())
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(gganimate)
 
 # ======================================================
 # banyak benda
 n_benda = 3
-
-# kita generate posisi masing-masing benda
-r_1 = sample(100,2,replace = T) 
-r_2 = sample(100,2,replace = T) 
-r_3 = sample(100,2,replace = T) 
-
-r_1
-r_2
-r_3
-
-# ======================================================
-# bikin grafik posisi awal
-x = c(r_1[1],r_2[1],r_3[1])
-y = c(r_1[2],r_2[2],r_3[2])
-
-data.frame(x,y) %>%
-  ggplot(aes(x = x,y = y)) +
-  geom_point() +
-  theme_void()
 
